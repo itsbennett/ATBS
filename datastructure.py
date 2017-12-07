992dc14cf5e4c6ef263eed70dc740184bc613593
Ben Mouton <email.bennett.here@gmail.com>
2017-12-06 23:59:32 -0500

    last
    last one for tonight

---
+++ datastructure.py
@@ -0,0 +1,11 @@
+# Data structures are powerful ways to organize data
+
+cat = {'name': 'Zophie', 'age': 7, 'color': 'gray'}
+allCats = []
+allCats.append({'name': 'Zophie', 'age': 7, 'color': 'gray'})
+allCats.append({'name': 'Pooka', 'age': 5, 'color': 'black'})
+allCats.append({'name': 'Fat-tail', 'age': 5, 'color': 'gray'})
+allCats.append({'name': '???', 'age': -1, 'color': 'orange'})
+
+print(allCats)
+# allCats is now a data structure
